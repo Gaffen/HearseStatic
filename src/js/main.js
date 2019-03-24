281e44424ec7664439eff4f5148c89f4b94e64b0
Gaffen <matt@gaffen.co.uk>
2019-03-24 08:45:31 +0000

    add record player component

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import GigWidget from "./components/GigWidget.vue";
+import RecordPlayer from "./components/RecordPlayer.vue";
 
 const gigs = new Vue({
   render: h => h(GigWidget)
-}).$mount("#nextshow");
+}).$mount(".giglink");
+
+const audio = new Vue({
+  render: h => h(RecordPlayer)
+}).$mount(".player");
