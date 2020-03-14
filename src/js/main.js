0ffa23d1cf890c27dbbd7224f34cf2c5ea2691dc
Gaffen <matt@gaffen.co.uk>
2020-03-14 17:43:57 +0000

    add carousel and video player to homepage

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import Modernizr from "./modernizr";
 // import GigWidget from "./components/GigWidget.vue";
 import RecordPlayer from "./components/RecordPlayer.vue";
 import lightbox from "./modules/lightbox";
+import Glide, { Autoplay, Keyboard, Controls } from '@glidejs/glide/dist/glide.modular.esm';
 
 // const gigs = new Vue({
 //   render: h => h(GigWidget)
@@ -54,6 +55,28 @@ Array.prototype.forEach.call(
     });
   }
 );
+
+const announcements = document.querySelectorAll('.announcements__slide');
+
+if(announcements.length > 1){
+  const carousel = new Glide('.announcements', {type: 'carousel', animationDuration: 1000, autoplay: 5000, gap: 0}).mount({Autoplay, Controls, Keyboard});
+
+  [...document.querySelectorAll('.announcements .videoplayer')]
+    .forEach((item, i) => {
+      if(!item.querySelector('.videoplayer__iframe')){
+        item.addEventListener('click', e => {
+          item.appendChild(
+            document
+              .createRange()
+              .createContextualFragment(`<div class="videoplayer__iframe">${item.dataset.video}</div>`)
+          );
+          carousel._o.autoplay = false;
+          // carousel.disable();
+        });
+      }
+    });
+}
+
 
 // const audio2 = new Vue({
 //   render: h => h(RecordPlayer)
