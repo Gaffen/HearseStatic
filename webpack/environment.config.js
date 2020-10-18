8f5ed3b36118b990da0549857deb04c97904e3e0
Matthew Gaffen <matt@sprtiz.co>
2020-10-18 21:51:59 +0100

    upgrades mostly complete; some remaining status errors to solve

--- webpack/environment.config.js
+++ webpack/environment.config.js
@@ -4,6 +4,8 @@ const WebpackAssetsManifest = require("webpack-assets-manifest");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const url = require("url");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 const path = require("path");
 const webpack = require("webpack");
@@ -15,20 +17,39 @@ function isProduction(env) {
 }
 
 function getPlugins(env) {
-  let plugins = [new webpack.ProgressPlugin({ profile: false })];
+  let plugins = [
+    new webpack.ProgressPlugin({ profile: false }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          [
+            "imagemin-svgo",
+            {
+              plugins: [
+                // SVGO options is here "https://github.com/svg/svgo#what-it-can-do"
+                {
+                  removeAttrs: ["fill", "stroke"],
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+  ];
   // console.log(env);
   return [
     new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: env == "development" ? "[name].css" : "[name]-[chunkhash].css",
-      chunkFilename: "[id].css"
+      chunkFilename: "[id].css",
     }),
     new WebpackAssetsManifest({
       output: "../../data/manifest.json",
       transform: (assets, manifest) => {
         let vueComponents = {};
-        Object.keys(manifest.compiler.records).forEach(e => {
+        Object.keys(manifest.compiler.records).forEach((e) => {
           if (e.includes(".vue?")) {
             const compName = e
               .match(/\/(.*)\.vue/)[1]
@@ -40,7 +61,7 @@ function getPlugins(env) {
         let output = {},
           re = /(?:\.([^.]+))?$/;
         output.vue = vueComponents;
-        Object.keys(assets).forEach(assetName => {
+        Object.keys(assets).forEach((assetName) => {
           const batchName = assetName.replace(/\.[^/.]+$/, ""),
             outputName = assets[assetName];
 
@@ -55,9 +76,9 @@ function getPlugins(env) {
           }
         });
         return output;
-      }
+      },
     }),
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
   ];
 }
 
@@ -76,9 +97,12 @@ module.exports = (config, env, target) => {
   // }
   config.plugins = getPlugins(env);
   config.externals = {
-    window: "window"
+    window: "window",
   };
-  config.optimization = { minimize: isProduction(env) };
+  config.optimization = {
+    minimize: isProduction(env),
+    minimizer: [new TerserPlugin()],
+  };
 
   return config;
 };
