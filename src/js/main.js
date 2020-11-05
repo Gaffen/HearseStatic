3227e1ee02c9e31d25e4b6cfd3797d1f94a7acbb
Matthew Gaffen <matt@sprtiz.co>
2020-11-05 13:34:17 +0000

    add presave integration

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,11 @@ import Modernizr from "./modernizr";
 // import GigWidget from "./components/GigWidget.vue";
 import RecordPlayer from "./components/RecordPlayer.vue";
 import lightbox from "./modules/lightbox";
-import Glide, { Autoplay, Keyboard, Controls } from '@glidejs/glide/dist/glide.modular.esm';
+import Glide, {
+  Autoplay,
+  Keyboard,
+  Controls,
+} from "@glidejs/glide/dist/glide.modular.esm";
 
 // const gigs = new Vue({
 //   render: h => h(GigWidget)
@@ -11,17 +15,17 @@ import Glide, { Autoplay, Keyboard, Controls } from '@glidejs/glide/dist/glide.m
 
 Array.prototype.forEach.call(
   document.querySelectorAll(".launch__lightbox"),
-  elem => {
+  (elem) => {
     lightbox(elem);
   }
 );
 
-let header = document.querySelector('.header__nav');
+let header = document.querySelector(".header__nav");
 
-if(header){
-  header.addEventListener('click', e => {
+if (header) {
+  header.addEventListener("click", (e) => {
     e.stopPropagation();
-    document.getElementById('header__checkbox').checked = false;
+    document.getElementById("header__checkbox").checked = false;
   });
 }
 
@@ -30,10 +34,10 @@ Array.prototype.forEach.call(
   document.querySelectorAll(".RecordPlayer"),
   function(elem) {
     new Vue({
-      render: h =>
+      render: (h) =>
         h(RecordPlayer, {
-          props:JSON.parse(elem.nextElementSibling.innerHTML)
-        })
+          props: JSON.parse(elem.nextElementSibling.innerHTML),
+        }),
     }).$mount(elem);
   }
 );
@@ -41,42 +45,50 @@ Array.prototype.forEach.call(
 
 Array.prototype.forEach.call(
   document.querySelectorAll(".fullpage__downarrow"),
-  elem => {
-    elem.addEventListener('click', e => {
+  (elem) => {
+    elem.addEventListener("click", (e) => {
       e.preventDefault();
-      const height = "innerHeight" in window
-               ? window.innerHeight
-               : document.documentElement.offsetHeight;
+      const height =
+        "innerHeight" in window
+          ? window.innerHeight
+          : document.documentElement.offsetHeight;
       window.scrollBy({
         top: height, // could be negative value
         left: 0,
-        behavior: 'smooth'
+        behavior: "smooth",
       });
     });
   }
 );
 
-const announcements = document.querySelectorAll('.announcements__slide');
+const announcements = document.querySelectorAll(".announcements__slide");
 
-if(announcements.length > 1){
-  const carousel = new Glide('.announcements', {type: 'carousel', animationDuration: 1000, autoplay: 5000, gap: 0}).mount({Autoplay, Controls, Keyboard});
+if (announcements.length > 1) {
+  const carousel = new Glide(".announcements", {
+    type: "carousel",
+    animationDuration: 1000,
+    autoplay: 5000,
+    gap: 0,
+  }).mount({ Autoplay, Controls, Keyboard });
 
-  [...document.querySelectorAll('.announcements .videoplayer')]
-    .forEach((item, i) => {
-      if(!item.querySelector('.videoplayer__iframe')){
-        item.addEventListener('click', e => {
+  [...document.querySelectorAll(".announcements .videoplayer")].forEach(
+    (item, i) => {
+      if (!item.querySelector(".videoplayer__iframe")) {
+        item.addEventListener("click", (e) => {
           item.appendChild(
             document
               .createRange()
-              .createContextualFragment(`<div class="videoplayer__iframe">${item.dataset.video}</div>`)
+              .createContextualFragment(
+                `<div class="videoplayer__iframe">${item.dataset.video}</div>`
+              )
           );
           carousel._o.autoplay = false;
           // carousel.disable();
         });
       }
-    });
+    }
+  );
 }
-
 
 // const audio2 = new Vue({
 //   render: h => h(RecordPlayer)
